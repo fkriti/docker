35726949baddf538b32d5363217cedf75e76c702
Kriti <53083330+fkriti@users.noreply.github.com>
2019-12-08 21:14:45 -0500

    Add files via upload

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import io
 import json
+import urllib
 from torchvision import models
 import torchvision.transforms as transforms
 from PIL import Image
@@ -13,25 +14,35 @@ model.eval()
 
 
 def transform_image(image_bytes):
-    my_transforms = transforms.Compose([transforms.Resize(255), transforms.CenterCrop(224), transforms.ToTensor(), transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+    my_transforms = transforms.Compose([transforms.Resize(255),
+                                        transforms.CenterCrop(224),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(
+                                            [0.485, 0.456, 0.406],
+                                            [0.229, 0.224, 0.225])])
     image = Image.open(io.BytesIO(image_bytes))
     return my_transforms(image).unsqueeze(0)
 
 
 def get_prediction(image_bytes):
-    outputs = model.forward(transform_image(image_bytes=image_bytes))
+    tensor = transform_image(image_bytes=image_bytes)
+    outputs = model.forward(tensor)
     _, y_hat = outputs.max(1)
-    classIdx = str(y_hat.item())
-    return imagenet_class_index[classIdx]
+    predicted_idx = str(y_hat.item())
+    return imagenet_class_index[predicted_idx]
 
 
-@server.route('/predict', methods=['POST'])
+@server.route('/predict', methods=['GET'])
 def predict():
-    if request.method == 'POST':
-        file = request.files['file']
-        img_bytes = file.read()
+    if request.method == 'GET':
+        url = request.args.get('url')
+        filename = "testing.jpg"
+        try: urllib.URLopener().retrieve(url, filename)
+        except: urllib.request.urlretrieve(url, filename)
+        img_bytes = open(filename,'rb').read()
         class_id, class_name = get_prediction(image_bytes=img_bytes)
-        return jsonify({'class_id': class_id, 'class_name': class_name})
+        return class_name
+
 
 if __name__ == '__main__':
     server.run()
